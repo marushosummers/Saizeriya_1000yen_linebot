a2395f33cc99ad989a74d4ad1b6b8d0796ef42b0
Sho <marusho@marusho.local>
2018-09-21 20:59:53 +0900

    fix

--- app.py
+++ app.py
@@ -72,13 +72,12 @@ def handle_message(event):
 
         line_bot_api.reply_message(event.reply_token,TextSendMessage(text=text.rstrip("\n")))
 
-
+    """
     else:
         text = 'ガチャボタンを押してね'
         print(text)
-
         line_bot_api.reply_message(event.reply_token,TextSendMessage(text=text)
-
+        """
 
 if __name__ == "__main__":
     app.run()
