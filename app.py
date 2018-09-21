ebb307496f83f038c7bbe3f748321c1b35cef781
Sho <marusho@marusho.local>
2018-09-21 17:49:30 +0900

    inline config

--- app.py
+++ app.py
@@ -15,7 +15,9 @@ from linebot.models import (
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config.from_object('config')
+#app.config.from_object('config')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sensai/saizeriya.db'
+app.config['SECRET_KEY'] = '\xf4\x1b.\x04\xba\xfc-c\x1cg\x8eV\xe9\xa6w}\xa7\xfc*zQnq\xed'
 
 db = SQLAlchemy(app)
 from sensai.views import get_menus
