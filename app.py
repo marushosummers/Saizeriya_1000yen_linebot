1810ca535b99f8ebdc98a545781c8e11a7ea44c9
Sho <marusho@marusho.local>
2018-09-21 17:13:36 +0900

    check 2times reply

--- app.py
+++ app.py
@@ -55,7 +55,9 @@ def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=event.message.text))
-
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
 
 if __name__ == "__main__":
     app.run()
