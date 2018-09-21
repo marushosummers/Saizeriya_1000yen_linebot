90573b38e415673fbeab44265c5ef633c98cdb2e
Sho <marusho@marusho.local>
2018-09-21 16:48:23 +0900

    fix app.py returncode

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ def callback():
     except InvalidSignatureError:
         abort(400)
 
-    return 'OK'
+    return {'statusCode': 200, 'body': '{}'}
 
 
 @handler.add(MessageEvent, message=TextMessage)
